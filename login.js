239749882f0609890d3f9562960e3dbc6528be0e
ara <araceli.cortes.20@gmail.com>
2019-11-06 05:10:24 -0600

    efectos con js

---
+++ login.js
@@ -0,0 +1,29 @@
+function Ingresa() {
+
+    document.getElementById("Ingresar").style.background = "#A937F0";
+    document.getElementById("Ingresar").style.opacity = "10";
+    document.getElementById("Ingresar").style.margin = " border: #A937F0 1.5px solid";
+
+    function envio() {
+        alert("sus datos han sido enviados correctamente");
+    }
+    envio();
+};
+
+function startup() {
+
+    document.getElementById("pest1").style.background = "#FFFFFF";
+    document.getElementById("pest1").style.border = " #ffffff .1px solid";
+    document.getElementById("pest1").style.color = "#A937F0";
+
+
+};
+
+function inversionista() {
+
+    document.getElementById("pest2").style.background = "#FFFFFF";
+    document.getElementById("pest2").style.border = " #ffffff .1px solid";
+    document.getElementById("pest2").style.color = "#A937F0";
+
+
+};
