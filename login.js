39dec004b2039ff7fd3ab6e214bcc94da0c1b013
ara <araceli.cortes.20@gmail.com>
2019-11-08 05:52:45 -0600

    bien pestañas

--- login.js
+++ login.js
@@ -1,36 +1,19 @@
-function Ingresa() {
+function eleccion() {
+    document.getElementById("pest2").style.background = "#FFFFFF";
+    document.getElementById("pest2").style.color = "#A937F0";
+    document.getElementById("pest2").style.border = "#FFFFFF .0px solid";
+    document.getElementById("pest1").style.background = "#A937F0";
+    document.getElementById("pest1").style.color = "#FFFFFF";
 
-    document.getElementById("Ingresar").style.background = "#A937F0";
-    document.getElementById("Ingresar").style.opacity = "10";
-    document.getElementById("Ingresar").style.margin = " border: #A937F0 1.5px solid";
 
-    function envio() {
-        alert("sus datos han sido enviados correctamente");
-    }
-    envio();
-};
+}
 
-function startup() {
-
+function eleccion1() {
     document.getElementById("pest1").style.background = "#FFFFFF";
-    document.getElementById("pest1").style.border = " #ffffff .1px solid";
     document.getElementById("pest1").style.color = "#A937F0";
+    document.getElementById("pest1").style.border = "#FFFFFF .0px solid";
     document.getElementById("pest2").style.background = "#A937F0";
     document.getElementById("pest2").style.color = "#FFFFFF";
-    document.getElementById("pest2").style.border = " #A937F0 .1px solid";
 
 
-
-};
-
-function inversionista() {
-
-    document.getElementById("pest2").style.background = "#FFFFFF";
-    document.getElementById("pest2").style.border = " #ffffff .1px solid";
-    document.getElementById("pest2").style.color = "#A937F0";
-    document.getElementById("pest1").style.background = "#A937F0";
-    document.getElementById("pest1").style.border = " #A937F0 .1px solid";
-    document.getElementById("pest1").style.color = "#ffffff";
-
-
-};
+}
